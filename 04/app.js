f84bbc2168b2a30f436da0bac815116833441188
Patryk <patryk.13@o2.pl>
2024-06-11 00:36:44 +0200

    Done task 04

--- 04/app.js
+++ 04/app.js
@@ -12,14 +12,28 @@ class App extends React.Component {
     }
 
     renderUsersList() {
-        const {users} = this.state;
-        return users.map(name => {
-            return (
-                <li onClick={ this.clickHandler }>
-                    { name }
-                </li>
-            );
-        });
+        const {users, searchQuery} = this.state;
+
+        if (searchQuery !== '') {
+            const filteredUsers = users.filter((user) => user.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+
+        return filteredUsers.map((name) => (
+            <li
+                key={name}
+                onClick={this.clickHandler}>
+                {name}
+            </li>
+        ));
+        } else {
+            return users.map((name) => (
+            <li
+                key={name}
+                onClick={this.clickHandler}>
+                {name}
+            </li>
+        ));
+        }
     }
 
     clickHandler = e => {
@@ -35,22 +49,31 @@ class App extends React.Component {
     }
 
     render() {
-        const { firstName, lastName } = this.state;
+        const {firstName, lastName, searchQuery} = this.state;
         return (
-            <section onSubmit={ this.submitHandler }>
-                <form>
-                    <input name="firstName"
-                        value={ firstName }
-                        onChange={ this.inputChange }
-                    />
-                    <input name="lastName"
-                        value={ lastName }
-                        onChange={ this.inputChange }
-                    />
-                    <input type="submit"/>
-                </form>
-                <ul>{ this.renderUsersList() }</ul>
-            </section>
+        <section>
+            <form onSubmit={this.submitHandler}>
+                <input
+                    name="firstName"
+                    value={firstName}
+                    onChange={this.inputChange}
+                />
+                <input
+                    name="lastName"
+                    value={lastName}
+                    onChange={this.inputChange}
+                />
+                <input type="submit" />
+            </form>
+    
+            <input
+                name="searchQuery"
+                value={searchQuery}
+                onChange={this.inputChange}
+            />
+    
+            <ul>{this.renderUsersList()}</ul>
+        </section>
         );
     }
 
